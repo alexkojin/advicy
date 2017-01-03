7cb61c6d4412743470e53671231a932addf0de96
Alex Kojin <alexkojin@gmail.com>
2017-01-03 08:50:30 +0300

    show most used tags

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,7 +1,7 @@
 module Api::V1
   class TagsController < ApiController
     def index
-      @tags = ActsAsTaggableOn::Tag.page(params[:page])
+      @tags = ActsAsTaggableOn::Tag.most_used.page(params[:page]).per(50)
     end
   end
 end
