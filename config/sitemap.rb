c099892ffd7de5da9bddbacaab8a5864e7d5eb55
Alex Kojin <alexkojin@gmail.com>
2017-02-05 22:59:50 +0300

    move a sitemap to s3

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,9 @@
-# Set the host name for URL creation
+
 SitemapGenerator::Sitemap.default_host = ENV['APP_URL']
+SitemapGenerator::Sitemap.sitemaps_host = "http://s3.amazonaws.com/sitemap-generator/"
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
 
 SitemapGenerator::Sitemap.create do
   Question.find_each do |q|
